8e80195e02a5155b053c10b7daaeece6552eefd1
Dmitry <idarefye@gmail.com>
2023-03-08 23:25:29 +0300

    add main fragment

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -1,11 +1,40 @@
 package com.example.weatherapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import com.example.weatherapp.databinding.ActivityMainBinding
+import com.example.weatherapp.fragments.MainFragment
+import org.json.JSONObject
+
+const val API_KEY = "fc28b19b4017409a865190956230703"
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.container, MainFragment.newInstance()).commit()
+    }
+
+
+    private fun getResult(name: String) {
+        val url = "https://api.weatherapi.com/v1/current.json?key=$API_KEY&q=$name&aqi=no"
+
+        val queue = Volley.newRequestQueue(this)
+        val stringRequest = StringRequest(com.android.volley.Request.Method.GET, url, { response ->
+            val obj = JSONObject(response)
+            val temp = obj.getJSONObject("current")
+            Log.d("Error", "Volley response :$temp")
+
+        }, { Log.d("Error", "Volley error :$it") })
+        queue.add(stringRequest)
     }
 }
